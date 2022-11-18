97a5cedc67108d39a2a4f0de6316f8f177ac56b0
zhyrin0 <zhyrin@tutanota.com>
2022-11-18 22:27:44 +0100

    Add TileDescriptors

---
+++ Source/towerdefense/Level/Tile/TileDescriptors.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UObject/SoftObjectPtr.h"
+#include "TileDescriptors.generated.h"
+
+USTRUCT()
+struct FTileDescriptor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	FString TileName;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TSoftObjectPtr<UStaticMesh>> Meshes;
+};
+
+UCLASS()
+class TOWERDEFENSE_API UTileDescriptors : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	TArray<FTileDescriptor> Tiles;
+};
