9e46337cf1b72a8001aacf47e60637ec69841ba1
zhyrin0 <zhyrin@tutanota.com>
2022-11-18 12:26:30 +0100

    Initial commit

---
+++ Source/towerdefense/towerdefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "towerdefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API AtowerdefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
