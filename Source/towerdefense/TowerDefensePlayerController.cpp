73bf6d33f10f54a9a0a6b39ee9de6b745ca57563
zhyrin0 <zhyrin@tutanota.com>
2022-11-22 22:14:39 +0100

    Enable mouse input

---
+++ Source/towerdefense/TowerDefensePlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TowerDefensePlayerController.h"
+
+ATowerDefensePlayerController::ATowerDefensePlayerController()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
