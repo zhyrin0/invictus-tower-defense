932664300ba2468ac438e485985cb2c2ce970cf9
zhyrin0 <zhyrin@tutanota.com>
2022-11-23 14:29:25 +0100

    Add default game state

---
+++ Source/towerdefense/TowerDefenseGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "TowerDefenseGameState.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_API ATowerDefenseGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	ATowerDefenseGameState();
+};
